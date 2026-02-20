1a5e98a84dbfd813ba2ad46233cdd356bcebe0d3
Aditya Dharma <agusadityadharma88@gmail.com>
2026-02-20 18:36:04 +0800

    feat(model) : Add ticket relation to Room and User model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ticket extends Model
 {
-    //
+    use HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'photo_path',
+        'admin_note'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
