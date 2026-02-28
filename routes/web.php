33d9840e09846ec1f8104fc43f303fdb36118c21
Aditya Dharma <agusadityadharma88@gmail.com>
2026-02-28 09:52:32 +0800

    feat(route) : update web.php for healt check

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Sadewas Hub API is rooning smoothly.',
+        'version' => '1.0.0',
+        'timestamp' => now()->toDateString()
+    ]);
 });
