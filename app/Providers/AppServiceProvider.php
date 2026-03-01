e0a8e8a63f7d646119dd8ca7f3a4c9c05c17dcd0
Aditya Dharma <agusadityadharma88@gmail.com>
2026-03-01 08:55:39 +0800

    fix: import facade Gate di AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
